9e3a92d4f3f169b94eb65998b1af0f1e01a5110f
mauriciosilv <mausilva828@gmail.com>
2020-03-10 20:32:52 -0300

    rows with redis

---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,35 @@
+'use strict'
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+class NewTaskMail {
+  static get concurrency () {
+    return 1
+  }
+
+  static get key () {
+    return 'NewTaskMail-job'
+  }
+
+  async handle ({ email, username, title, file }) {
+    console.log(`Job: ${NewTaskMail.key}`)
+    await Mail.send(
+      ['new_task'],
+      { username, title, hasAttachment: !!file },
+      message => {
+        message
+          .to(email)
+          .from('mausilva828@gmail.com', 'Mauricio da Silva')
+          .subject('Nova tarefa para você')
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name
+          })
+        }
+      }
+    )
+  }
+}
+
+module.exports = NewTaskMail
