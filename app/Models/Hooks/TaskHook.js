81795c5ad7cdab20d22a8939c9b2b14e8aa07054
mauriciosilv <mausilva828@gmail.com>
2020-03-10 17:48:33 -0300

    create hook of task

---
+++ app/Models/Hooks/TaskHook.js
@@ -0,0 +1,31 @@
+'use strict'
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+const TaskHook = exports = module.exports = {}
+
+TaskHook.sendNewTaskMail = async (taskInstance) => {
+  if (!taskInstance.user_id && !taskInstance.dirty.user_id) return
+
+  const { email, username } = await taskInstance.user().fetch()
+  const file = await taskInstance.file().fetch()
+
+  const { title } = taskInstance
+
+  await Mail.send(
+    ['new_task'],
+    { username, title, hasAttachment: !!file },
+    message => {
+      message
+        .to(email)
+        .from('mausilva828@gmail.com', 'Mauricio da Silva')
+        .subject('Nova tarefa para você')
+
+      if (file) {
+        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+          filename: file.name
+        })
+      }
+    }
+  )
+}
